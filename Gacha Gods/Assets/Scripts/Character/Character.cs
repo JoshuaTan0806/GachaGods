9646b0bfc763b36be0c3e9e88ce1f4b9974130e3
Joshua Tan <joshuatan0806@gmail.com>
2022-07-21 00:09:30 +0930

    automated setting up scriptables

--- Gacha Gods/Assets/Scripts/Character/Character.cs
+++ Gacha Gods/Assets/Scripts/Character/Character.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 using Sirenix.OdinInspector;
+using UnityEditor;
 
 [CreateAssetMenu(menuName = "Character/Character")]
 public class Character : ScriptableObject
@@ -20,10 +22,50 @@ public class Character : ScriptableObject
     [SerializeField] StatFloatDictionary baseStats;
     public List<Mastery> Mastery => mastery;
     [SerializeField] List<Mastery> mastery;
+    public Attack Attack => Attack;
+    [SerializeField] Attack attack;
+    public Spell Spell => Spell;
+    [SerializeField] Spell spell;
 
     [Button]
     void CreateScriptables()
     {
+        string path = AssetDatabase.GetAssetPath(this);
+        int lastSlash = 0;
 
+        for (int i = 0; i < path.Length; i++)
+        {
+            if (path[i] == '/')
+                lastSlash = i;
+        }
+
+        path = path.Remove(lastSlash + 1); 
+
+        if(!File.Exists(path + name + " Attack.asset"))
+        {
+            Attack attack = CreateInstance<Attack>();
+            AssetDatabase.CreateAsset(attack, path + name + " Attack.asset");
+            this.attack = attack;
+        }
+
+        if (!File.Exists(path + name + " Spell.asset"))
+        {
+            Spell spell = CreateInstance<Spell>();
+            AssetDatabase.CreateAsset(spell, path + name + " Spell.asset");
+            this.spell = spell;
+        }
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (!File.Exists(path + name + " Mastery " + (i+1) + ".asset"))
+            {
+                Mastery mastery = CreateInstance<Mastery>();
+                AssetDatabase.CreateAsset(mastery, path + name + " Mastery " + (i + 1) + ".asset");
+                this.mastery.Add(mastery);
+            }
+        }
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
     }
 }
